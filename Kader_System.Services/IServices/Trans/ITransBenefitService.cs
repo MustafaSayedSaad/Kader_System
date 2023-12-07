c02fe18d4660b9c09a2fe33a4f0b10c6102d1830
Admin <Admin@DESKTOP-B0OTIDE>
2023-12-07 17:00:42 +0200

    Last changes for all pro

---
+++ Kader_System.Services/IServices/Trans/ITransBenefitService.cs
@@ -0,0 +1,12 @@
+﻿namespace Kader_System.Services.IServices.Trans;
+
+public interface ITransBenefitService
+{
+    Task<Response<IEnumerable<StSelectListForMainScreenCategoryResponse>>> ListOfMainScreensCategoriesAsync(string lang);
+    Task<Response<StGetAllMainScreensCategoriesResponse>> GetAllMainScreensCategoriesAsync(string lang, StGetAllFiltrationsForMainScreenCategoryRequest model);
+    Task<Response<StCreateMainScreenCategoryRequest>> CreateMainScreenCategoryAsync(StCreateMainScreenCategoryRequest model);
+    Task<Response<StGetMainScreenCategoryByIdResponse>> GetMainScreenCategoryByIdAsync(int id);
+    Task<Response<StUpdateMainScreenCategoryRequest>> UpdateMainScreenCategoryAsync(int id, StUpdateMainScreenCategoryRequest model);
+    Task<Response<string>> UpdateActiveOrNotMainScreenCategoryAsync(int id);
+    Task<Response<string>> DeleteMainScreenCategoryAsync(int id);
+}
